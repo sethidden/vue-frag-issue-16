1c441c9b8c2e44e9879fa4c1d96a4d1e5af0d5f3
Artur Tagisow <artur.tagisow@billennium.com>
2021-09-06 14:34:22 +0200

    simpler repro

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <MyInput :visible="toggler2" />
-    <MyInput :visible="toggler2" />
     <MyInput :visible="toggler" />
+    <div v-if="toggler" />
   </div>
 </template>
 
@@ -19,11 +18,10 @@ export default {
   data() {
     return {
       toggler: false,
-      toggler2: true,
     }
   },
   mounted() {
-    setInterval(() => {this.toggler = !this.toggler; this.toggler2 = !this.toggler2}, 1)
+    setInterval(() => {this.toggler = !this.toggler;}, 100)
   }
 }
 </script>
